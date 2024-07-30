50ac60e81b67402fea2ca8350c958582a975abec
zaynab <zeinabmohammed817@gmail.com>
2024-07-30 08:29:37 +0300

    enhace create table function, fix insert function

--- Function/TableConnect/create.sh
+++ Function/TableConnect/create.sh
@@ -2,6 +2,7 @@
 
 Create_fun(){
 
+ flag="No"
  read -p "Enter your Table Name:" TableName
  array=( $(ls ./Databases/$1/))
  typearray=( "int" "string" )
@@ -15,29 +16,43 @@ Create_fun(){
 		touch "${TableName}.metadata"
 		read -p "Enter coloumns number:" cnum
                 echo "Column_number: $cnum" >> "${TableName}.metadata"
+		
 		for((i=1;i<=${cnum};i++))
 		do
 	            read -p "Enter coloumn $i name :" cname
+		    if [[ "$flag" == "No" ]]
+	            then
+			echo "hello"		    
+     		    	## Ask user if want this column to act as a primary_key
+		    	read -p "IF U want this column to be a primary key Enter[Y/N]: " choice
+			if [[ "$choice" == "Y" || "$choice" == "y" ]]
+		        then
+				flag="Yes"
+		        fi		    
+		    fi
 		    ## append columns names inside table in same line
 		    printf "$cname " >> "${TableName}"
-		    echo  "For_type enter int OR string"
-                    
-		    read -p "Enter coloumn $i type :" Type
-		    #echo "$Type"
-		    ## give user another try to enter type if he entered a wrong type
+		    echo  "For_type enter [int OR string]"
+                    read -p "Enter coloumn $i type :" Type
+		   	
+		    ######################################################################
+		     ## give user another try to enter type if he entered a wrong type
 		    if [[ !(${typearray[@]} =~ $Type) ]]
 		    then
-			echo  "For_type enter int OR string"
+			echo  "For_type enter [int OR string]"
 			read -p "Enter coloumn $i type :" Type
-		    else
-			echo "$cname $Type" >> "${TableName}.metadata"
-			continue
 		    fi	
-		    
-		    echo "$cname $Type" >> "${TableName}.metadata"
+		    ######################################################################
+		   if [[ "$flag" == "Yes" ]]
+		   then 
+			echo "$cname $Type primarykey" >> "${TableName}.metadata"
+	           else  	
+		        echo "$cname $Type" >> "${TableName}.metadata"
+		   fi	
 		done
 		#to append new line 
 		echo " " >> "${TableName}"
+
 	 else
 		echo "Invalid!! Try, Again"
 		echo "It starts with number!! OR contains special_char!!"
--- Function/TableConnect/insert.sh
+++ Function/TableConnect/insert.sh
@@ -2,46 +2,68 @@
 
 source ./Validation/CheckIntType.sh
 source ./Validation/CheckStringType.sh
-user_input(){
-read "Enter your value to $1 with type $2:" value
-return $value
 
+unique_value_fun () {
+
+  ####will write code here
 }
 
 Insert_fun(){
 read -p "Enter your Table Name:" TableName
 cd ./Databases/$1
-while read c1 c2
+col_arry=()
+type_arry=()
+primary_key=()
+
+while read c1 c2 c3
 do
-	echo "$c1"
 	if [[ "$c1" == "Column_number:" ]]
-	then	
-		continue
-	else	
-		echo "$c2"	
-		#read -p "Enter your value to ${c1} with type ${c2}:" value
-		value=user_input "$c1" "$c2"
-		if [[ "$c2" == "int" ]]
-		then 
-			var=$(CheckIntType_fun "$value")
-			if [[ "$var" == "1" ]]
-			then
-				printf "$value " >> "${TableName}"
-			else
-				echo "TRY, AGAIN!!! The value doesn't contain only numbers"
-			fi	
-		#elif [[ $c2 == "string" ]]
-	        else
-		       var=$(CheckStringType_fun "$value")
-	       		if [[ "$var" == "1" ]]
-                        then
-                                printf "$value " >> "${TableName}"
-                        else
-                                echo "TRY, AGAIN!!! The value doesn't contain only chracters"
-                        fi	       
-		fi	
-	fi			
-done < "${TableName}.metadata"
+        then
+                continue
+	elif [[ "$c3" == "primarykey" ]]
+	then		
+		primary_key+=($c1)
+	else
+		col_arry+=($c1)	
+		type_arry+=($c2)
+        fi
+
+done < "${TableName}.metadata"	
+
+#echo "${primary_key[@]}"
+#echo "${type_arry[@]}"
+
+###### insert into Table ##########
+for ((i=0;i<${#col_arry[@]};i++))
+do       
+read -p "Enter your value to ${col_arry[i]} column with type ${type_arry[i]}:" value
+#### Check if current col is primary key or not #######
+if [[ "${col_arry[i]}"  == "${primary_key[0]}" ]]
+then
+	#check if value is unique
+	unique_value_fun "$value"
+fi
+if [[ "${type_arry[i]}"  == "int" ]]
+then 
+	var=$(CheckIntType_fun "$value")
+	if [[ "$var" == "1" ]]
+	then
+		printf "$value " >> "${TableName}"
+	else
+		echo "TRY, AGAIN!!! The value doesn't contain only numbers"
+	fi
+else
+	var=$(CheckStringType_fun "$value")
+	if [[ "$var" == "1" ]]
+        then
+        	printf "$value " >> "${TableName}"
+        else
+       		echo "TRY, AGAIN!!! The value doesn't contain only chracters"
+        fi	       
+fi	
+done
 	
 echo " " >> "${TableName}"
 }
+
+
