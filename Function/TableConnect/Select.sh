5de74511be1307555a94a6f7f6fb22afb26ce11c
Ammar <ammarelghazali@gmail.com>
2024-08-07 13:36:10 +0300

    enhance select func

--- Function/TableConnect/Select.sh
+++ Function/TableConnect/Select.sh
@@ -2,19 +2,33 @@
 
 Select_fun()
 {
+  # cd ./Databases/$1
+    ls ./Databases/$1
  read -p "Enter your Table Name:" TableName
  array=($(ls ./Databases/$1))
- 
 if [[ !(${array[@]} =~ $TableName) ]]
 then
     echo "not exist"
 else
-set -x
     cd ./Databases/$1
-    ls
-     read -p "Select by:" sby
-     awk  '{for ( i=0 ; i<=NF ; i++) if ($i=="id") {print $0}}' $TableName
-set +x
+   # ls
+       select item in "Select All" "Select By Any Field In Table" "exit"
+do 
+	case $item in
+	       	"Select All")
+                awk '{print $0}' $TableName
+ 
+			;;
+	   	"Select By Any Field In Table")
+			     read -p "Select by:" sby
+                              awk -v var="${sby}"  '{IGNORECASE = 1; for ( i=0 ; i<=NF ; i++) if ($i==var) {print $0}}' $TableName
+ 
+                	;;
+		"exit")
+			exit
+      			;;		
+	esac
+done
 fi
-
+cd ../../
 }
